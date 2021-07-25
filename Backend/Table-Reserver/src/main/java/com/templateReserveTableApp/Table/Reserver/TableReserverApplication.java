465ba31644283a9785e272f8404a2292b019f3ed
shaho93 <shahriyar93@outlook.de>
2021-07-25 21:18:53 +0200

    First Commit

---
+++ Backend/Table-Reserver/src/main/java/com/templateReserveTableApp/Table/Reserver/TableReserverApplication.java
@@ -0,0 +1,13 @@
+package com.templateReserveTableApp.Table.Reserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableReserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableReserverApplication.class, args);
+	}
+
+}
---
+++ Backend/Table-Reserver/src/test/java/com/templateReserveTableApp/Table/Reserver/TableReserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.templateReserveTableApp.Table.Reserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableReserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
