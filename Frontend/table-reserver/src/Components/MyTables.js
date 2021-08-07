33440f419f06613eb6a9e3e12bb1679f5f74e1dc
shaho93 <shahriyar93@outlook.de>
2021-08-07 19:58:56 +0200

    Added Tables that are divided into two columns

---
+++ Frontend/table-reserver/src/Components/MyTables.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+function MyTables(){
+
+    return(
+        <div className="container">
+            <h2 className="MyTexts"> Available Tables</h2>
+            
+            <div className="TablesContainer">
+                <div className="TableItem">
+                <button className="OneTable"> Table 1</button>
+                </div>
+                <div className="TableItem">
+                <button className="OneTable"> Table 2</button>
+                </div>
+                <div className="TableItem">
+                <button className="OneTable"> Table 3</button>
+                </div>
+                <div className="TableItem">
+                <button className="OneTable"> Table 4</button>
+                </div>
+                <div className="TableItem">
+                <button className="OneTable"> Table 5</button>
+                </div>
+                <div className="TableItem">
+                <button className="OneTable"> Table 6</button>
+                </div>
+                                
+            </div>
+
+        </div>
+
+    )
+}
+
+function onClickTables(){
+
+}
+
+
+export default MyTables;
---
+++ Frontend/table-reserver/src/Components/WelcomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function WelcomePage(){
+    return(
+        <div className = "container">
+            <h1 className="MyTexts">Welcome to Restaurant ABC</h1>
+
+            <p id="PleaseReserveText"> Please choose a table reserve</p>
+        </div>
+    )
+}
+
+export default WelcomePage;
