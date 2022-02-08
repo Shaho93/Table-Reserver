2ade3792630129b89b8475a02ec3b2146eb6a001
shaho93 <shahriyar93@outlook.de>
2022-02-08 19:55:56 +0100

    Modified the functions

--- Frontend/table-reserver/src/Components/MyTables.js
+++ Frontend/table-reserver/src/Components/MyTables.js
@@ -1,42 +1,66 @@
 import React from 'react';
+import Popup from 'react-popup';
 
 
-function MyTables(){
 
-    return(
-        <div className="container">
-            <h2 className="MyTexts"> Available Tables</h2>
-            
-            <div className="TablesContainer">
-                <div className="TableItem">
-                <button className="OneTable"> Table 1</button>
+class MyTables extends React.Component{
+
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            value: this.props.defaultValue,
+            nameOfCustomer: "",
+            dateOfReservation: ""
+        }
+
+        this.clickReserveTable = this.onClickTables.bind(this);
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        if(prevState.value !== this.state.value){
+            this.props.onClickTables(this.state.value);
+        }
+    }
+
+    onClickTables(e){
+        console.log("Table reserved");
+        console.log(e);
+        
+    }
+
+    render(){
+    
+        return(
+            <div className="container">
+                <h2 className="MyTexts"> Available Tables</h2>
+                
+                <div className="TablesContainer">
+                    <div className="TableItem">
+                    <button className="OneTable"> Table 1</button>
+                    </div>
+                    <div className="TableItem">
+                    <button className="OneTable"> Table 2</button>
+                    </div>
+                    <div className="TableItem">
+                    <button className="OneTable" onClick = {this.clickReserveTable}> Table 3</button>
+                    </div>
+                    <div className="TableItem">
+                    <button className="OneTable" onClick = {this.clickReserveTable}> Table 4</button>
+                    </div>
+                    <div className="TableItem">
+                    <button className="OneTable"> Table 5</button>
+                    </div>
+                    <div className="TableItem">
+                    <button className="OneTable"> Table 6</button>
+                    </div>
+                                    
                 </div>
-                <div className="TableItem">
-                <button className="OneTable"> Table 2</button>
-                </div>
-                <div className="TableItem">
-                <button className="OneTable"> Table 3</button>
-                </div>
-                <div className="TableItem">
-                <button className="OneTable"> Table 4</button>
-                </div>
-                <div className="TableItem">
-                <button className="OneTable"> Table 5</button>
-                </div>
-                <div className="TableItem">
-                <button className="OneTable"> Table 6</button>
-                </div>
-                                
             </div>
 
-        </div>
+        );
+    }
 
-    )
+    
 }
-
-function onClickTables(){
-
-}
-
-
 export default MyTables;
